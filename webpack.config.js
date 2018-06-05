3fcfbb2c8916caccbee6a27e0fdb1ff9b9bbf3f7
alocke12992 <alocke12992@gmail.com>
2018-06-05 15:40:58 -0600

    Adds babel dependencies

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    'single-spa.config': 'src/root-application/single-spa.config.js',
+  },
+  output: {
+    publicPath: '/dist/',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    modules: [
+      __dirname,
+      'node_modules',
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ContextReplacementPlugin(
+      /(.+)?angular(\\|\/)core(.+)?/,
+      path.resolve(__dirname, '../src')
+    )
+  ],
+  devtool: 'source-map',
+  externals: [],
+  devServer: {
+    historyApiFallback: true
+  }
+}; 
