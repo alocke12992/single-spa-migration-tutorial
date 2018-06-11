e5e40b2443f1236c6e52e16bc426c8932577db1d
alocke12992 <alocke12992@gmail.com>
2018-06-11 11:04:37 -0600

    Fixes routing error

--- pokedex/src/components/Search.js
+++ pokedex/src/components/Search.js
@@ -1,12 +1,7 @@
 import React from 'react'
 
 const Search = ({ onChange }) => (
-  // <input
-  //   type="text"
-  //   onChange={onChange}
-  //   placeholder="Enter pokemon name..."
-  // />
-  <div class="input-field col s6">
+  <div className="input-field col s6">
     <input placeholder="Ender a pokemon name..." onChange={onChange} id="pokemonName" label="Pokemon Search" type="text" className="validate" />
   </div>
 )
