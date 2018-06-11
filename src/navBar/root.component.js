9aa77fba03f2e0af1ae3c2a8e699b1a42a5cc7fa
alocke12992 <alocke12992@gmail.com>
2018-06-11 15:09:51 -0600

    Adds huge-apps react example

--- src/navBar/root.component.js
+++ src/navBar/root.component.js
@@ -18,7 +18,7 @@ function menuItems() {
     <div>
       <li><a onClick={() => navigateToUrl('/')}>Home</a></li>
       <li><a onClick={() => navigateToUrl('/pokedex')}>Pokemon</a></li>
-      <li><a onClick={() => navigateToUrl('/emoji')}>Emoji</a></li>
+      <li><a onClick={() => navigateToUrl('/modal_gallery')}>Gallery</a></li>
     </div>
   )
 }
--- src/root-application/single-spa.config.js
+++ src/root-application/single-spa.config.js
@@ -3,7 +3,7 @@ import { registerApplication, start } from 'single-spa';
 registerApplication('navBar', () => import('../navBar/navBar.app.js'), () => true);
 registerApplication('home', () => import('../home/home.app.js'), () => location.pathname === "" || location.pathname === "/");
 registerApplication('pokedex', () => import('../../pokedex/pokedex.app.js'), pathPrefix('/pokedex'));
-registerApplication('emoji', () => import('../../emoji-search/emoji.app.js'), pathPrefix('/emoji'));
+registerApplication('modal_gallery', () => import('../../modal_gallery/gallery.app.js'), pathPrefix('/modal_gallery'));
 
 
 start();
